4ed049cce3bb7c287f46abb5747029a757225c46
Alex Chiu <alexdchiu@gmail.com>
2023-01-08 15:39:45 -0500

    feat: add property slice

--- apps/properties/serializers.py
+++ apps/properties/serializers.py
@@ -7,6 +7,7 @@ class PropertySerializer(serializers.ModelSerializer):
   user = serializers.SerializerMethodField()
   country = CountryField(name_only=True)
   cover_photo = serializers.SerializerMethodField()
+  profile_photo = serializers.SerializerMethodField()
   photo1 = serializers.SerializerMethodField()
   photo2 = serializers.SerializerMethodField()
   photo3 = serializers.SerializerMethodField()
@@ -17,6 +18,7 @@ class PropertySerializer(serializers.ModelSerializer):
     fields = [
       'id',
       'user',
+      "profile_photo",
       'title',
       'slug',
       'ref_code',
@@ -61,6 +63,9 @@ class PropertySerializer(serializers.ModelSerializer):
   
   def get_photo4(self, obj):
     return obj.photo4.url
+  
+  def get_profile_photo(self, obj):
+    return obj.user.profile.profile_photo.url
 
 class PropertyCreateSerializer(serializers.ModelSerializer):
   country = CountryField(name_only=True)
